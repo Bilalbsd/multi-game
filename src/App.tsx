ef6710d54ef2f07901d6ad0ee67a1fa177b704f8
BilalBsd <dev.bilalb@gmail.com>
2024-09-19 19:58:45 +0200

    feat: add menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
+import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import FlagGuesser from "./games/FlagGuesser/flag-guesser";
 import TicTacToe from "./games/TicTacToe/tic-tac-toe";
+import { FaGamepad, FaFlag } from "react-icons/fa";
+import "./style.css";
+
+const Home = () => (
+  <div className="home-container">
+    <h1 className="home-title">MultiGames</h1>
+    <div className="game-buttons">
+      <Link to="/tictactoe" className="game-button">
+        <FaGamepad className="game-icon" />
+        <span>Tic Tac Toe</span>
+      </Link>
+      <Link to="/flagguesser" className="game-button">
+        <FaFlag className="game-icon" />
+        <span>Devinez le drapeau</span>
+      </Link>
+    </div>
+  </div>
+);
 
 export default function App() {
   return (
-    <>
-      <TicTacToe />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/tictactoe" element={<TicTacToe />} />
+        <Route path="/flagguesser" element={<FlagGuesser />} />
+      </Routes>
+    </Router>
   );
 }
